65d7e1f76a03d94d4d4152b07db0c401b6263737
Akramtello <35160721+AkramTT@users.noreply.github.com>
2021-03-18 09:25:06 +0800

    use require. parms
    require.parms is good to use it always when we need to sent params hash for a security reason

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,9 +13,16 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new
-    @article.title = params[:article][:title]
-    @article.body=params[:article][:body]
+    #@article.title = params[:article][:title]
+    #@article.body=params[:article][:body]
     @article.save
     redirect_to article_path(@article)
   end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+  
 end
